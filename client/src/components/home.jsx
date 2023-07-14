bd066313fb38c328c0926bcc6e76de876d65aeb9
raviacts035 <ravindrainturi035@gmail.com>
2023-07-14 23:17:29 +0530

    functionality is ok

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,11 +2,26 @@ import { useState } from "react"
 
 const Home=()=>{
     const [number,setNumber]=useState()
+    const [countryCode,setCountryCode]=useState(91)
+    const [dialog,setDialog]=useState(false);
 
-const handleSubmit=(eve)=>{
-    eve.preventDefault();
-    console.log(number)
-}
+    const handleSmsOTP=async (eve, mode)=>{
+        eve.preventDefault();
+        if (!number || !countryCode) return
+        let url=''
+        if (mode==='sms'){
+            url='http://127.0.0.1:5000'+'/otp/sms/+'+countryCode+number;
+        }
+        else if (mode==='voice'){
+            url='http://127.0.0.1:5000'+'/otp/voice/+'+countryCode+number;
+        }
+        var resp=await fetch(url)
+        var data=await resp.json()
+        console.log(data)
+        if (data.success) setDialog(true)
+        return
+    }
+
 
     return (
         <section>
@@ -17,15 +32,21 @@ const handleSubmit=(eve)=>{
             <form>
                 <div>
                     <p>Country code</p>
-                    <input type="+91" value={+91}/>
+                    <input type='text' value={countryCode} onChange={(event)=>{setCountryCode(event.target.value)}} placeholder="country"/>
                 </div>
                 <div>
                     <p>Eneter mobile Number</p>
-                    <input type='number' value={number} onChange={(event)=>{setNumber(event.target.value)}} placeholder="mobile"/>
+                    <input type='text' value={number} onChange={(event)=>{setNumber(event.target.value)}} placeholder="mobile"/>
                 </div>
                 <div>
-                    <button onClick={(eve)=>{handleSubmit(eve)}}>send OTP</button>
+                    <button onClick={(eve)=>{handleSmsOTP(eve,'sms')}}>send SMS</button>
+                    <button onClick={(eve)=>{handleSmsOTP(eve,'voice')}}>send Voice</button>
                 </div>
+                {dialog && 
+                <div>
+                    <p>OTP sent sucessfully to : +{countryCode} {number}</p>
+                </div>
+                }
             </form>
         </div>
         </section>
